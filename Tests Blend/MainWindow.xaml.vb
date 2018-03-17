47dcd62d2b1b5a9128146636533437a17dcd400c
Laurent85 <manceaulaurent@yahoo.fr>
2018-03-17 09:56:17 +0100

    Ajoutez des fichiers projet.

---
+++ Tests Blend/MainWindow.xaml.vb
@@ -0,0 +1,8 @@
+﻿Class MainWindow 
+
+
+	Private Sub Clique(ByVal sender as Object, ByVal e as System.Windows.RoutedEventArgs)
+		bouton1.Content="test"
+		bouton1.ToolTip="gf"
+	End Sub
+End Class
---
+++ Tests Blend/My Project/AssemblyInfo.vb
@@ -0,0 +1,59 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Globalization
+Imports System.Resources
+Imports System.Windows
+
+' Les informations générales relatives à un assembly sont contrôlées via le jeu d'attributs 
+' suivant. Changez ces valeurs d'attribut pour modifier les informations
+' associées à un assembly.
+
+' Révisez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("Tests_Blend")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Tests_Blend")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: ComVisible(false)>
+
+'Pour commencer à construire des applications localisables, placez 
+'<UICulture>CultureYouAreCodingWith</UICulture> de votre fichier .vbproj
+'à l'intérieur d'un groupe <PropertyGroup>. Par exemple, si vous utilisez l'anglais US 
+'dans vos fichiers source, réglez <UICulture> sur "en-US".  Puis, annulez la mise en commentaire
+'de l'attribut NeutralResourceLanguage ci-dessous. Actualisez "en-US" dans la ligne
+'ci-dessous pour qu'elle corresponde au réglage de UICulture dans le fichier de projet.
+
+'<Assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)> 
+
+
+'L'attribut ThemeInfo indique où peuvent être trouvés les éventuels dictionnaires de ressources spécifiques et génériques.
+'1er paramètre : emplacement des dictionnaires de ressources spécifiques au thème
+'(utilisé si une ressource n'est pas trouvée dans les dictionnaires de ressource 
+' de la page ou de l'application)
+
+'2e paramètre : emplacement du dictionnaire de ressources générique
+'(utilisé si une ressource n'est pas trouvée dans les dictionnaires de ressource 
+'de la page, de l'application ou spécifiques au thème)
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
+
+
+
+'Le GUID suivant concerne l'ID de la bibliothèque de type (typelib) si ce projet est exposé à COM
+<Assembly: Guid("d00ce2bb-81d3-44a4-a5cc-029f8097a66c")> 
+
+' Les informations de version pour un assembly comprennent les 4 valeurs suivantes :
+'
+'      Version majeure
+'      Version mineure 
+'      Numéro de création
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou laisser les valeurs par défaut des numéros de création et de révision 
+' en utilisant le caractère '*' comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
